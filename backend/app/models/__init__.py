ccf2100b4a9d41afadb377bf23268e3b5b38b4d1
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-10-31 13:49:23 -0300

    [US-1.1] chore: User model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,1 +1,9 @@
-# Empty __init__.py file to make this directory a Python package
+"""
+Models package for the application.
+
+This package contains all database models used by the application.
+"""
+
+from .user import User
+
+__all__ = ["User"]
